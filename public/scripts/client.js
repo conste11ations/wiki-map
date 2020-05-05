f46d697a3203661c4fb870e2b78f6177b2a12da1
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-05-05 21:29:32 +0000

    renamed it from app.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,44 +1,12 @@
-$(document).ready(function() {
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'pk.eyJ1IjoiYW1yaGFtYWRhIiwiYSI6ImNrOW9wMjU3OTAyeXkzZ3FzZ3E0aHQ3dGsifQ.X4zjaqxq3wEeLdAIgVOE6A'
-  }).addTo(mymap);
-
-  var marker = L.marker([51.5, -0.09]).addTo(mymap);
-  marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-  console.log(mymap);
-
-  // bike lanes
-  L.tileLayer('http://tiles.mapc.org/trailmap-onroad/{z}/{x}/{y}.png',
-  {
-    maxZoom: 17,
-    minZoom: 9
-  }).addTo(map);
-
-  $(() => {
-    $.ajax({
-      method: "GET",
-      url: "/api/users"
-    }).done((users) => {
-      for(user of users) {
-        $("<div>").text(user.name).appendTo($("body"));
-      }
-    });;
-  });
-});
-
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   if (users){
+  //     for(user of users) {
+  //       $("<div>").text(user.name).appendTo($("body"));
+  //     }
+  //   }
+  // });;
 });
