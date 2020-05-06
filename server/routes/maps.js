f4d21d6edf7446397a9c061742dd46c919e3aff0
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-05-06 20:19:22 +0000

    added post method for creating new maps

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -3,10 +3,51 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM map_points;`)
+
+    let query;
+    if (req.query.city && req.query.category) {
+      query = `SELECT * FROM maps where city = '${req.query.city}' AND category = '${req.query.category}'`;
+    } else if (req.query.city) {
+      query = `SELECT * FROM maps where city = '${req.query.city}'`;
+    } else if (req.query.category) {
+      query = `SELECT * FROM maps where category = '${req.query.category}'`;
+    } else {
+      query = `SELECT * FROM maps`;
+    }
+    console.log(query);
+    db.query(query)
       .then(data => {
         const maps = data.rows;
-        console.log(maps)
+        res.json({ maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    let query = `SELECT * FROM maps WHERE id = ${req.params.id}`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const maps = data.rows;
+        res.json({ maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id/map_points", (req, res) => {
+    let query = `SELECT * FROM map_points WHERE map_id = ${req.params.id}`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const maps = data.rows;
         res.json({ maps });
       })
       .catch(err => {
@@ -17,14 +58,13 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
-    // console.log(req.body.items)
     if (!req.body.items) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
     const items = req.body.items
-    const mapQuery = `INSERT INTO maps (user_id, city) VALUES ($1, $2) RETURNING *;`;
-    const mapValues = [1, 'toronto'];
+    const mapQuery = `INSERT INTO maps (user_id, city, category) VALUES ($1, $2, $3) RETURNING *;`;
+    const mapValues = [1, 'toronto','Food'];
     db.query(mapQuery, mapValues)
     .then(dbRes => {
       const mapID = dbRes.rows[0].id
@@ -34,13 +74,10 @@ module.exports = (db) => {
     })
     .then(dbRes => {
       const maps = dbRes.rows;
-      // console.log(maps)
         res.json({ maps })
     })
     .catch( err => console.log(err))
   });
-
-  router.get("/:id/maps/new")
 
   return router;
 };
