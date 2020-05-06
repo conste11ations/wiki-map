60272d53462ab1355d7044d01a43663ae2851671
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-05-06 20:18:46 +0000

    changed lat and long for a simple layer instead for points

--- server/db/schema/03_map_points.sql
+++ server/db/schema/03_map_points.sql
@@ -1,9 +1,7 @@
 DROP TABLE IF EXISTS map_points CASCADE;
 CREATE TABLE map_points (
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
-  layers VARCHAR(10000) NOT NULL,
-  -- longitude NUMERIC NOT NULL,
-  -- latitude NUMERIC NOT NULL,
+  layers VARCHAR(400) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description varchar(255),
   image varchar(255),
