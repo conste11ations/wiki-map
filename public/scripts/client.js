77ba23550a1b8f20db4d7618747a211732cabc51
Rachel Kat <rachelkat@gmail.com>
2020-05-06 00:03:55 +0000

    modularized createFavoritesButton and cleaned up other functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
       data: {
         user_id: userId,
         map_id: mapId
-      }
+      },
     }).then(result => {
 
     });
@@ -40,30 +40,34 @@ $(document).ready(function () {
     });
   }
 
+  const createFavoritesButton = (userId, mapObj) => {
+    const favoritesButton = L.easyButton({
+      states: [{
+        stateName: 'not-starred',
+        icon: `<span class="star" id="${mapObj._container.id}">&starf;</span>`,
+        title: 'Not favorited',
+        onClick: (btn, map) => {
+          btn.state('yellow-starred');
+          addFavorite(userId, map._container.id.substring(3));
+        }
+      }, {
+        stateName: 'yellow-starred',
+        icon: `<span class="star-yellow" id="${mapObj._container.id}">&starf;</span>`,
+        title: 'Is favorited',
+        onClick: (btn, map) => {
+          btn.state('not-starred');
+          removeFavorite(userId, map._container.id.substring(3));
+        }
+      }]
+    });
+    favoritesButton.addTo(mapObj);
+  }
+
   // loads the user's stars for a given map list (yellow denotes favorited by user, black is otherwise)
   const loadFavorites = (userId, mapList) => {
 
     $.each(mapList, (key, value) => {
-      const favoritesButton = L.easyButton({
-        states: [{
-                stateName: 'not-starred',
-                icon:      `<span class="star" id="${key}">&starf;</span>`,
-                title:     'Not favorited',
-                onClick: (btn, map, user) => {
-                    btn.state('yellow-starred');
-                    addFavorite(current_user, map._container.id.substring(3));
-                }
-            }, {
-                stateName: 'yellow-starred',
-                icon:      `<span class="star-yellow" id="${key}">&starf;</span>`,
-                title:     'Is favorited',
-                onClick: (btn, map, user) => {
-                    btn.state('not-starred');
-                    removeFavorite(current_user, map._container.id.substring(3));
-                }
-        }]
-      });
-      favoritesButton.addTo(value);
+      createFavoritesButton(userId, value);
     });
 
     $.ajax({
@@ -72,8 +76,8 @@ $(document).ready(function () {
     }).then(result => {
 
       $.each(result.maps, (key, value) => {
-        if (mapList['map' + value.map_id]) {
-          removeFavorite(current_user, value.map_id);
+        if (mapList['map' + value.map_id] && $(`span.star#map${value.map_id}`)) {
+          removeFavorite(userId, value.map_id);
           $(`span.star#map${value.map_id}`).click();
         }
       });
@@ -81,7 +85,7 @@ $(document).ready(function () {
 
   };
 
-  const loadMaps = (city, category) => {
+  const loadMaps = (userId, city, category) => {
     $.ajax({
       method: 'GET',
       url: '/api/maps',
@@ -96,9 +100,7 @@ $(document).ready(function () {
       $.each(result.maps, (key, value) => {
 
         const mapId = `map${value.id}`;
-
         $('.maps-list').append(`<div id='${mapId}' class='map' data-city='${value.city}' data-category='${value.category}'></div>`);
-
         const mymap = L.map(mapId).setView([value.center_lat, value.center_long], 13);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -118,22 +120,22 @@ $(document).ready(function () {
         loadMarkers(value.id, mymap);
         mapList[mapId] = mymap;
       });
-// TODO needs to be refactored once the concept of authenticated users is introduced (remove hardcoded param of 1)
-      loadFavorites(current_user, mapList);
+
+      loadFavorites(userId, mapList);
     });
   };
 
-  loadMaps(undefined, undefined);
+  loadMaps(current_user, undefined, undefined);
 
   // landing page filtering
-  $('form').on('submit', function(event) {
-      event.preventDefault();
-      $('.maps-list').html('');
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    $('.maps-list').html('');
 
-      const $city = $(this).find('input').val();
-      const $category = $(this).find('option:selected').val().toLowerCase();
+    const $city = $(this).find('input').val();
+    const $category = $(this).find('option:selected').val().toLowerCase();
 
-    $category === 'all categories' ? loadMaps(`${$city}`, undefined) : loadMaps(`${$city}`, `${$category}`);
+    $category === 'all categories' ? loadMaps(current_user, `${$city}`, undefined) : loadMaps(current_user, `${$city}`, `${$category}`);
   });
 
 
