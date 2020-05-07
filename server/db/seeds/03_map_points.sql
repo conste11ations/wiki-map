75d0af629f4cbde0b0ba9430a0af4553e0d0e00a
Rachel Kat <rachelkat@gmail.com>
2020-05-07 00:54:17 +0000

    add map points seed data

--- server/db/seeds/03_map_points.sql
+++ server/db/seeds/03_map_points.sql
@@ -1,19 +1,17 @@
--- INSERT INTO map_points (map_id, latitude, longitude, title, description, image, website) values
--- (
---   1,
---   43.6510114,
---   -79.3706424,
---   'Ardo Restaurant',
---   'great food',
---   'https://lh5.googleusercontent.com/p/AF1QipPvRMNU8myJQfvYrmoRhauMTBa7lBfcLKuBD970=w408-h271-k-no',
---   'http://www.ardorestaurant.com/'
--- ),
--- (
---   1,
---   43.7468333,
---   -79.4099305,
---   'Auberge Du Pommier',
---   'super expensive',
---   'https://lh5.googleusercontent.com/p/AF1QipMMrNBG3OoXSpI_1AyOjgkng8j3BHJfihVA17Eo=w408-h270-k-no',
---   'https://www.aubergedupommier.com/'
--- );
+INSERT INTO map_points (map_id, layers, title, description, image, website) VALUES
+(
+  1,
+  '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[43.6510114, -79.3706424]}}',
+  'Ardo Restaurant',
+  'great food',
+  'https://lh5.googleusercontent.com/p/AF1QipPvRMNU8myJQfvYrmoRhauMTBa7lBfcLKuBD970=w408-h271-k-no',
+  'http://www.ardorestaurant.com/'
+),
+(
+  1,
+  '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[43.7468333, -79.4099305]}}',
+  'Auberge Du Pommier',
+  'super expensive',
+  'https://lh5.googleusercontent.com/p/AF1QipMMrNBG3OoXSpI_1AyOjgkng8j3BHJfihVA17Eo=w408-h270-k-no',
+  'https://www.aubergedupommier.com/'
+);
