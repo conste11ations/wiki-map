01f92abc795568f6e29fb8dcea0e5457f9bbcd0c
Rachel Kat <rachelkat@gmail.com>
2020-05-07 00:46:35 +0000

    removed widgets

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,4 @@
 INSERT INTO users (name, email, city, profile_image, password) VALUES
-('Alice', '@amr', 'toronto', 'https://i.imgur.com/73hZDYK.png', 'password');
-('Kira', '@hamada', 'toronto', 'https://i.imgur.com/3GvwNBf.png', 'password'),
 ('Alison Jones', 'a1@mail.com', 'Toronto', 'https://i.imgur.com/73hZDYK.png', 'password'),
 ('Rodney Descartes', 'rd@mail.com', 'Toronto', 'https://i.imgur.com/5fUVPRP.png', 'password'),
 ('Isaac Newton', 'in@mail.com', 'Montreal', 'https://i.imgur.com/nlhLi3I.png', 'password'),
@@ -10,4 +8,6 @@ INSERT INTO users (name, email, city, profile_image, password) VALUES
 ('Lucy Liu', 'l1@mail.com', 'Toronto', 'https://i.imgur.com/73hZDYK.png', 'password'),
 ('Mads Mikkelsen', 'mm@mail.com', 'Toronto', 'https://i.imgur.com/5fUVPRP.png', 'password'),
 ('Hugh Jackman', 'hj@mail.com', 'Toronto', 'https://i.imgur.com/3GvwNBf.png', 'password'),
-('Iam McKellen', 'im@mail.com', 'Vancouver', 'https://i.imgur.com/73hZDYK.png', 'password');
+('Iam McKellen', 'im@mail.com', 'Vancouver', 'https://i.imgur.com/73hZDYK.png', 'password'),
+('Alice', '@amr', 'Toronto', 'https://i.imgur.com/73hZDYK.png', 'password'),
+('Kira', '@hamada', 'Toronto', 'https://i.imgur.com/3GvwNBf.png', 'password');
--- server/db/seeds/02_maps.sql
+++ server/db/seeds/02_maps.sql
@@ -1,8 +1,8 @@
--- INSERT INTO maps (user_id, city, category, center_lat, center_long) values
--- (1, 'Toronto', 'food', 43.651070, -79.347015),
--- (1, 'Montreal', 'food', 45.508888, -73.561668),
--- (2, 'Toronto', 'accomodations', 43.651070, -79.347015),
--- (3, 'Montreal', 'shopping', 45.508888, -73.561668),
--- (4, 'New York City', 'attractions', 	40.730610, -73.935242),
--- (5, 'Toronto', 'attractions', 43.651070, -79.347015),
--- (6, 'Chicago', 'food', 41.881832, -87.623177);
+INSERT INTO maps (user_id, city, category, center_lat, center_long) VALUES
+(1, 'Toronto', 'food', 43.651070, -79.347015),
+(1, 'Montreal', 'food', 45.508888, -73.561668),
+(2, 'Toronto', 'accomodations', 43.651070, -79.347015),
+(3, 'Montreal', 'shopping', 45.508888, -73.561668),
+(4, 'New York City', 'attractions', 	40.730610, -73.935242),
+(5, 'Toronto', 'attractions', 43.651070, -79.347015),
+(6, 'Chicago', 'food', 41.881832, -87.623177);
